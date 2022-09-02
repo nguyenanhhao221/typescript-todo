0749ba043d68f374ad563888b603c799a5daa6f6
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-02 17:18:31 +0700

    feat: :sparkles: add Edit Mode to edit content of the ToDo
    Alow to edit todo content when click on the edit button icon, update the state properly
    
    Need to work on auto focus on the text of todo after click on edit button

--- src/components/SingleToDo.tsx
+++ src/components/SingleToDo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToDoList } from '../App';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -10,6 +10,7 @@ type Props = {
 };
 
 const SingleToDo = ({ todo, todoList, setTodoList }: Props) => {
+  const [editMode, setEditMode] = useState(false);
   //handleDone
   //Whenever user clicked on the done button on single todo, go through the todoList array, change any todo object with id matched then changes isDone property.
   const handleDone: (todoId: number) => void = todoId => {
@@ -24,18 +25,47 @@ const SingleToDo = ({ todo, todoList, setTodoList }: Props) => {
   const handleDelete: (todoId: number) => void = todoId => {
     setTodoList(todoList.filter(todo => todo.id !== todoId));
   };
+
+  //Set up text in todo to display based on isDOne or Edit mode
+  let todoText: JSX.Element = <span className='todo-text'>{todo.todo} </span>;
+  if (todo.isDone) {
+    todoText = <s className='todo-text'>{todo.todo}</s>;
+  } else if (!todo.isDone && editMode) {
+    todoText = (
+      <input
+        type='text'
+        name='todo-text'
+        id='todo-text'
+        value={todo.todo}
+        placeholder='Edit Todo'
+        onChange={e => {
+          setTodoList(
+            todoList.map(eachTodo =>
+              eachTodo.id === todo.id
+                ? { ...eachTodo, todo: e.target.value }
+                : eachTodo
+            )
+          );
+        }}
+        required
+        className='todo-text bg-transparent focus:outline-none w-full'
+      />
+    );
+  }
+
   return (
-    <form className="single-todo flex justify-between p-4 rounded-md w-full bg-[url('https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg')]">
-      {/* Check is isDone is true, if yes render the straight through, otherwise render normal span text */}
-      {todo.isDone ? (
-        <s className='todo-text'>{todo.todo}</s>
-      ) : (
-        <span className='todo-text'>{todo.todo} </span>
-      )}
+    <form
+      className="single-todo flex justify-between p-4 rounded-md w-full bg-[url('https://img.freepik.com/free-photo/crumpled-yellow-paper-background-close-up_60487-2390.jpg?size=626&ext=jpg')]"
+      onSubmit={e => {
+        e.preventDefault();
+        return;
+      }}
+    >
+      {todoText}
 
       <div className='flex items-center gap-x-1'>
         <span className='icon cursor-pointer'>
-          <AiFillEdit />
+          <AiFillEdit onClick={() => setEditMode(!editMode)} />
         </span>
         <span className='icon cursor-pointer'>
           <AiFillDelete onClick={() => handleDelete(todo.id)} />
